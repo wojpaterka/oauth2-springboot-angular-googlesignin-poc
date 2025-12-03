9879800447db07b55ac08e27a3901d3540dc34e3
Wojtek <wojpat1@st.amu.edu.pl>
2025-12-03 19:10:35 +0100

    fix(okr3): fix checkstyle config and resource-server test

--- resource-server/src/test/java/com/example/SmokeTest.java
+++ resource-server/src/test/java/com/example/SmokeTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SmokeTest {
+
     @Test
     void testBasic() {
         assertTrue(true);
     }
 }
-EOF
